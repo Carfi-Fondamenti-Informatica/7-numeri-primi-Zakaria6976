369699bfacc8276805ac1e740dce348080149a02
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-11-30 15:43:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool ric(int i, int n){
+    
+    if(i==n){
+        return true;
+    }
+    else if(n%i==0){
+        return false;
+    }
+    else {
+        return ric(i+1,n);
+    }
+}
