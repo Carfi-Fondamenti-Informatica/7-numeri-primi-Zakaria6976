3ac4b97b69bb773eaf992d9ae856cc6a1df65cd3
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-11-30 15:44:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int n;
+int main() {
+    cin>>n;
+    if(ric(2,n)) {
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
